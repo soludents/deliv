842b5a8d23de2c7d0c0400e75a551dba0598d4d5
Ben Abitbol <Benjiab@hotmail.com>
2019-04-16 01:41:55 +0300

    bug fix

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,21 +1,26 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from "@angular/core";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  selector: "app-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.scss"]
 })
 export class ButtonComponent {
-
   @Input() title: string;
   @Input() icon: string;
   @Input() floatRight: boolean;
   @Input() disabled: boolean;
 
-  @Output() click = new EventEmitter<boolean>();
+  @Output() onButtonClicked = new EventEmitter<boolean>();
 
   onButtonClick(event) {
-    this.click.emit(event);
+    this.onButtonClicked.emit(event);
   }
 }
--- src/app/shared/components/store-item-detail/store-item-detail.component.ts
+++ src/app/shared/components/store-item-detail/store-item-detail.component.ts
@@ -1,13 +1,18 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from "@angular/core";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: 'app-store-item-detail',
-  templateUrl: './store-item-detail.component.html',
-  styleUrls: ['./store-item-detail.component.scss']
+  selector: "app-store-item-detail",
+  templateUrl: "./store-item-detail.component.html",
+  styleUrls: ["./store-item-detail.component.scss"]
 })
 export class StoreItemDetailComponent {
-
   @Input() name: string;
   @Input() description: string;
   @Input() imageUrl: string;
